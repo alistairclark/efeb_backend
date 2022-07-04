afb1ca3e135be2e72f005f9b2dd16a30bab384c6
Alistair Clark <alistairclark89@gmail.com>
2022-07-04 14:20:03 +0100

    First pass at models

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'efeb_backend.orders'
+    'efeb_backend.stock_items'
 ]
 
 MIDDLEWARE = [
---
+++ efeb_backend/stock_items/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    display_name = models.CharField(max_length=255)
+
+
+class Category(models.Model):
+    display_name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class StockItem(models.Model):
+    display_name = models.CharField(max_length=1000)
+    price = models.DecimalField(decimal_places=2, max_digits=7)
+    picture = models.ImageField()
+    stock_count = models.IntegerField()
+    description = models.TextField()
+    categories = models.ManyToManyField(Category)
+    manufacturer = models.ForeignKey(Manufacturer, null=True, on_delete=models.SET_NULL)
+    links = models.TextField()
