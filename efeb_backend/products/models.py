285cb472566d045d1876dec9283bcfe3fda6eb03
Alistair Clark <alistairclark89@gmail.com>
2022-07-05 19:29:22 +0100

    Add __str__ methods

--- efeb_backend/products/models.py
+++ efeb_backend/products/models.py
@@ -5,6 +5,9 @@ class Manufacturer(models.Model):
     display_name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
 
+    def __str__(self):
+        return self.display_name
+
 
 class Category(models.Model):
     display_name = models.CharField(max_length=255)
@@ -12,6 +15,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.display_name
 
 
 class Product(models.Model):
@@ -24,3 +30,6 @@ class Product(models.Model):
     manufacturer = models.ForeignKey(Manufacturer, null=True, on_delete=models.SET_NULL)
     links = models.TextField()
     slug = models.SlugField(max_length=255)
+
+    def __str__(self):
+        return self.display_name
