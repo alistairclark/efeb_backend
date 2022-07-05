098c38d5f1181d6d6644cb06b206b8b5ed8dba53
Alistair Clark <alistairclark89@gmail.com>
2022-07-05 19:05:27 +0100

    Add static dir

--- config/settings/base.py
+++ config/settings/base.py
@@ -21,7 +21,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "../efeb_backend/static"),)
 
 
 # Quick-start development settings - unsuitable for production
