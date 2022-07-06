d4cec4bd724c4ecb1b5996e31b760e938e5ec35a
Alistair Clark <alistairclark89@gmail.com>
2022-07-06 12:25:45 +0100

    Add sentry

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,3 +1,6 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from config.settings.base import *
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
@@ -15,3 +18,17 @@ AWS_S3_REGION_NAME = "eu-west-2"
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+sentry_sdk.init(
+    dsn="https://e200202f7d8e4c66a73cb3a7ea07a4e7@o1307791.ingest.sentry.io/6552533",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+)
