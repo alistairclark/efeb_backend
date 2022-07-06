182e083e587857ef530515bef5ee7064e946c23a
Alistair Clark <alistairclark89@gmail.com>
2022-07-06 12:38:08 +0100

    Remove some S3 settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,9 +13,7 @@ ALLOWED_HOSTS = [
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("S3_BUCKET")
-AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = "eu-west-2"
-AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
