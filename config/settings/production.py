4a88406134682f3cecea0a1b1adcf679a631148d
Alistair Clark <alistairclark89@gmail.com>
2022-08-02 12:34:54 +0100

    Adds sendgrid config

--- config/settings/production.py
+++ config/settings/production.py
@@ -37,3 +37,12 @@ sentry_sdk.init(
 
 # Frontend URL
 FRONTEND_URL = "https://efeb.store/"
+
+# Sendgrid
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
+
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST_USER = "apikey"  # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
